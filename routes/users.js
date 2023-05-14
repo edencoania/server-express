620092aeea3c4f629c280c799c3e634d511cb505
Eden Coania <edencoania@gmail.com>
2023-05-15 00:59:02 +0300

    json

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 router.get('/log', function(req, res, next) {
-  res.status(200).send('eden is the king!');
+  const data = { message: 'eden is the king!' };
+  res.status(200).json(data);
 });
-
 
 module.exports = router;
