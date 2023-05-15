39121c165536ada7903589695f2c44e1c8423c00
Eden Coania <edencoania@gmail.com>
2023-05-15 14:04:50 +0300

    new try

--- app.js
+++ app.js
@@ -1,29 +1,61 @@
-const express = require("express");
+var createError = require('http-errors');
+var express = require("express");
+
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const PORT = process.env.PORT || 8000;
 const cors = require("cors");
+
 const bodyParser = require('body-parser');
-const app = express();
-const path = require('path');
-const usersDAL = require(path.join(__dirname, '.', 'DAL', 'usersDAL'));
+const jwt = require('jsonwebtoken');
+const url = require('url');
+
+var usersDAL = require(path.join(__dirname, '.', 'DAL', 'usersDAL'));
 //const usersDAL = require('./DAL/usersDAL');
 var teamsRouter = require('./routes/teams');
 var usersRouter = require('./routes/users');
 var eventsRouter = require('./routes/events');
-const jwt = require('jsonwebtoken');
-const url = require('url');
-const PORT = process.env.PORT || 8000;
+var loginRouter = require('./routes/login');
+var secrets = require('./key/secretKey');
 
+var app = express();
 
-const loginRouter = require('./routes/login');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-const secrets = require('./key/secretKey');
 
 app.use(cors());
+app.use(logger('dev'));
+
 app.use(express.json());
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/login', loginRouter);
 app.use('/teams', teamsRouter);
 app.use('/users', usersRouter);
 app.use('/events', eventsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	next(createError(404));
+  });
+
+  // error handler
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+  });
 
 
 app.get("/message", (req, res) => {
@@ -61,8 +93,7 @@ else{
 	}
   });
 
-  app.listen(PORT, () => {
-	console.log(`server started on port ${PORT}`);
-  });
-
-  module.exports = app;
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
+module.exports = app;
